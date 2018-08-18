016ef7ad59f812203a0a78d24c83918e35e47ca1
이상민 <dltkdals43z@naver.com>
2018-08-18 21:25:25 +0900

    리스트 새로고침했을때 안뜨는 오류 수정

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,64 +1,81 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  FlatList
+  FlatList,
+  View
 } from 'react-native';
 import { initUserlist, getUserlist, getCurrentUserInfo} from '../actions';
 import { UserList } from '../UserList';
-import { CardImage } from '../components';
+import { CardImage, Spinner } from '../components';
 import { connect } from 'react-redux';
-
 class List extends Component {
 
   state = {
       refreshing: false,
-      data: this.props.userlist
+      isLoading: false,
+      data: [],
+      error: '',
     }
-    onEndReached = () => {
-     this.setState(state => ({
-       data: [
-         ...state.data,
-         ...this.props.userlist,
-       ]
-     }));
+    onEndReached = async () => {
+    await this.props.getUserlist();
    };
 
-   onRefresh = () => {
-     this.setState({
-       data: this.props.userlist,
-     });
+   onRefresh = async () => {
+    await this.props.getUserlist();
    }
 
-   async componentWillMount(){
-     this.props.initUserlist();
-     await this.props.getUserlist();
-     await this.props.getCurrentUserInfo();
+   renderFlatList(isLoading) {
+     return (<FlatList
+      data={this.state.data}
+      initialNumToRender={1}
+      onEndReachedThreshold={1}
+      onEndReached={this.onEndReached}
+      refreshing={this.state.refreshing}
+      onRefresh={this.onRefresh}
+      keyExtractor={this._keyExtractor}
+      renderItem={({ item }) => {
+        return (
+          <CardImage
+            source={{ uri: item.profileUrl }}
+            nickname={item.nickname}
+            city={item.city}
+            sex={item.sex}
+            number={item.number}
+            selfIntro={item.selfIntro}
+            age={item.age}
+          />
+        );
+      }}
+    />)
    }
+
+   componentDidMount() {
+        this.props.initUserlist();
+        this.setState({ isLoading: true }, async () => {
+            await this.props.getUserlist();
+            await this.props.getCurrentUserInfo();
+        });
+    }
+
+    componentWillReceiveProps(props){
+      const { userlist, userInfo, error } = props;
+      console.log(userInfo ,userlist)
+        if (userlist.length >0) {
+            this.setState({data: userlist, isLoading: false})
+        }
+    }
+   _keyExtractor(item, index) {
+     return item.nickname
+   }
+
 
 
   render() {
     return (
-        <FlatList
-         data={this.props.userlist}
-         initialNumToRender={1}
-         onEndReachedThreshold={1}
-         onEndReached={this.onEndReached}
-         refreshing={this.state.refreshing}
-         onRefresh={this.onRefresh}
-         renderItem={({ item }) => {
-           return (
-             <CardImage
-               source={{ uri: item.profileUrl }}
-               nickname={item.nickname}
-               city={item.city}
-               sex={item.sex}
-               number={item.number}
-               selfIntro={item.selfIntro}
-               age={item.age}
-             />
-           );
-         }}
-       />
+      <View>
+        <Spinner visible={this.state.isLoading} />
+        {this.renderFlatList()}
+      </View>
     );
   }
 }
