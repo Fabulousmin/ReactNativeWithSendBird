f66663b0221ba207288f2e768c55e56579a53a23
이상민 <dltkdals43z@naver.com>
2018-08-18 21:28:18 +0900

    ''

--- src/subyeonActions/list.js
+++ src/subyeonActions/list.js
@@ -1,12 +1,5 @@
 import firebase from 'firebase';
 
-// export const sGetUserlist () => {
-//   return new Promise((resolve, reject) => {
-//     const storageRef = firebase.Storage.ref();
-//     const usersRef = storageRef.child('users')
-//   })
-// }
-
 
 export const sGetUserlist = () =>  {
   return new Promise ((resolve, reject) => {
@@ -15,13 +8,10 @@ export const sGetUserlist = () =>  {
      usersRef.on('child_added',(snap)=> {
        const { sex, age, nickname, profileUrl, city, number, selfIntro } = snap.val();
        const profile = { sex, age, nickname, profileUrl, city, number, selfIntro };
+       console.log(userlist);
        userlist.push(profile);
      })
-    if(userlist){
-      console.log(userlist);
-      resolve(userlist);
-    }
-      reject(error);
+     if(userlist) {resolve(userlist);}
    }
- )
+  )
 }
