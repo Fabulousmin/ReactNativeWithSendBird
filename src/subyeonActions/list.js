778e6cc72669c7d6aeacb7e9f2a767c61b8648c2
이상민 <dltkdals43z@naver.com>
2018-08-20 12:25:14 +0900

    error 메시지 추가

--- src/subyeonActions/list.js
+++ src/subyeonActions/list.js
@@ -1,17 +1,33 @@
 import firebase from 'firebase';
 
 
+
 export const sGetUserlist = () =>  {
   return new Promise ((resolve, reject) => {
-   const usersRef = firebase.database().ref().child("users");
+    const usersRef = firebase.database().ref().child("users");
     const userlist = [];
      usersRef.on('child_added',(snap)=> {
-       const { sex, age, nickname, profileUrl, city, number, selfIntro } = snap.val();
-       const profile = { sex, age, nickname, profileUrl, city, number, selfIntro };
+       const { sex, age, nickname, profileUrl, city, number, selfIntro, uid, updatedAt } = snap.val();
+       const profile = { sex, age, nickname, profileUrl, city, number, selfIntro, uid, updatedAt };
        console.log(userlist);
        userlist.push(profile);
+       resolve(userlist);
      })
-     if(userlist) {resolve(userlist);}
    }
+  )
+}
+
+
+
+export const sOnPressLike = (uid, myNickname) => {
+  return new Promise ((resolve, reject) => {
+    const userRef = firebase.database().ref('users/' + uid +'/like');
+    const like = { nickname: myNickname,
+                   createdAt:Date(),
+                 }
+    userRef.push(like)
+    .then(() => {console.log('like'); resolve(like); })
+    .catch(() => {console.log('like fail'); reject('like fail')})
+    }
   )
 }
